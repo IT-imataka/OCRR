b7480c01507e59f7a59a54bfd8b52dbaaa7cae7f
IT-imataka <takashi6128_gakky@outlook.com>
2025-06-15 07:31:21 +0900

    canvas要素のスタイルを修正し、グリッド描画ロジックを改善してOCR機能を強化

--- public/ocr-capacity/index.js
+++ public/ocr-capacity/index.js
@@ -9,31 +9,66 @@ const ocrimg = (event) => {
   // console.log(imgfile.width);
   //アップ画像のURLを生成
   img.src = URL.createObjectURL(imgfile);
-  // console.log(img.width);
+  // img要素に画像が読み込まれたかを確認する用
+  // 　console.log(img.width);
   const drawgird = () => {
-    const cardwidth = 67.5;
-    const cardheight = 100;
+    const cardwidth = 65;
+    const cardheight = 25;
     const beginx = 537.5;
     const beginy = 235;
     const gapx = 10;
-    const gapy = 5;
+    const gapy = 80;
     const rows = 4;
     const cols = 10;
+    const maindeck = [];
+    const exdeck = [];
 
+    //gridのスタイル
     ctx.strokeStyle = "red";
     ctx.lineWidth = 2;
 
+    // grid生成
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < cols; j++) {
         const x = beginx + (cardwidth + gapx) * j;
         const y = beginy + (cardheight + gapy) * i;
+        // grid描画
         ctx.strokeRect(x, y, cardwidth, cardheight);
+
+        // メモリ上だけで切り出した各カードをcanvasに描画
+        const maindeckcan = document.createElement("canvas");
+        maindeckcan.width = cardwidth;
+        maindeckcan.height = cardheight;
+        const maindeckcanctx = maindeckcan.getContext("2d");
+        maindeckcanctx.drawImage(
+          canvas, // 描画元
+          x, //切り抜くx座標
+          y, //切り抜くy座標
+          cardwidth, //切り抜き幅
+          cardheight, //切り抜き高さ
+          0, //配置座標
+          0, //配置座標
+          cardwidth, //配置サイズ
+          cardheight //配置サイズ
+        );
+        maindeck.push(maindeckcan);
       }
     }
+    const ocrcards = async function () {
+      for (const card of maindeck) {
+        const result = await Tesseract.recognize(card, "eng", {
+          logger: () => {},
+        });
+        // const {data:{text:text}} = result;
+        const text = result.data.text;
+        console.log(text);
+      }
+    };
+    ocrcards();
   };
 
   img.onload = () => {
-    console.log(ctx);
+    // console.log(ctx);
     canvas.width = img.width;
     canvas.height = img.height;
 
@@ -42,7 +77,13 @@ const ocrimg = (event) => {
 
     Tesseract.recognize(canvas, "jpn", {
       logger: (m) => {
-        const num = Math.floor(m.progress * 1000);
+        const progress = m.progress;
+        const num = Math.floor(progress * 1000);
+        const progressbar = document
+          .createElement("progress")
+          .setAttribute("value", "progress");
+        output.innerText = `${progress / 10}%`;
+        output.appendChild(progressbar);
         document.getElementById("output").innerText = `${num / 10}%`;
       },
     }).then(({ data: { text } }) => {
